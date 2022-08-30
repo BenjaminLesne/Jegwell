f3e27c9353095be68a0ebc82b52fccb181f54b0d
Benjamin LESNE <benjamin.lesne@outlook.fr>
2022-08-30 19:00:09 +0200

    feat:JG-12 installation de sentry
    ## Quel est la fonctionnalité:
    1-Installation de Sentry.io
    2-Envoie d'un email lorsque l'initialisation de Sentry renvoie une erreur.
    3-Créer des logs d'erreurs en même temps.
    
    ## Pourquoi cela est utile ?
    1-Afin de tracker les erreurs coté client
    2-Afin d'être sûr que Sentry se lance
    3-Afin d'être vraiment sûr que Sentry s'initialise
    
    ## Comment je l'ai implémenté :
    J'ai utilisé la méthode init de la classe Sentry comme indiqué par la documentation Sentry. (https://docs.sentry.io/platforms/php/)
    J'ai utilisé PHPMailer pour les mails. (https://github.com/PHPMailer/PHPMailer)
    Pour les logs j'ai utilisé les fonctionnalité natives de PHP (création/modification de fichier)
    
    Maintenant, nous sommes sûr que nous pouvons tracker les erreurs coté clients.

---
+++ wordpress/wp-content/themes/Jegwell-front/tests/SentryTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+$root_directory_level = 5;
+require_once dirname(__DIR__, $root_directory_level) . '/vendor/autoload.php';
+require_once dirname(__DIR__, $root_directory_level) . '/wordpress/jegwell-functions.php';
+
+/** @desc this instantiates Dotenv and passes in our path to .env */
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, $root_directory_level));
+$dotenv->load();
+
+use PHPUnit\Framework\TestCase;
+
+use function Jegwell\functions\addToLogs;
+use function Jegwell\functions\initializeSentry;
+
+
+class jegwellFuntionsTest extends TestCase
+{
+    // Sentry 
+    public function testSentryWrongDsn()
+    {
+        $inputs = array(123, "345", true, false, null);
+        foreach ($inputs as $input) {
+            $result = initializeSentry($input);
+            $this->assertSame($result, array("status" => "fail", "email" => "success"));
+        }
+    }
+
+    public function testSentryInitialization(): void
+    {
+        // Initialise Sentry.io avec PHP
+        $initialization = \Sentry\init(['dsn' => $_ENV['SENTRY_DSN']]);
+        $this->assertSame($initialization, null);
+    }
+
+    public function testaddToLogs()
+    {
+        $randomId = rand();
+        $message = "this is my error message {$randomId}";
+        $type = 'test-error';
+        $logPath  = dirname(__DIR__, 5) . "/logs/{$type}-logs";
+        $previousContent = file_exists($logPath) ? file_get_contents($logPath) : "default";
+
+        addToLogs('test-error', $message);
+
+        $currentContent = file_get_contents($logPath);
+
+        $this->assertStringContainsString($message, $currentContent);
+
+        if ($previousContent !== "default") {
+            $this->assertStringContainsString($previousContent, $currentContent);
+        }
+    }
+}
