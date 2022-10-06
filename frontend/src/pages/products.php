2afdcc887e3584b6732496e3ca98cab3b2764492
Benjamin LESNE <benjamin.lesne@outlook.fr>
2022-10-06 12:24:57 +0200

    feat:JG-40 coder la page des produits
    ## Quel est la fonctionnalité:
    1-La page des produits est maintenant codé, on peut filtrer les résultats avec la sélection d'une catégorie ainsi qu'un type de trie (A-Z, Z-A, etc.)
    2-Le bouton 'ajouter au panier', créé un cookie contenant l'id et la quantité du produit ajouté (ces cookies seront utilisé dans la page panier)
    3-La pagination est affiché sous condition qu'il y a + d'une page de produits soit + de 10 produits retourné par l'api
    4-ajout d'un champ 'Variants' dans sanity/le backend
    5-installation de Sentry et dotenv (problème pour les importé -> a fixer)
    6-mise à jour du style de la bannière d'erreur
    7-création de modals pour la sélection des catégories et du type de trie des résultat
    8-mise à jour de tsconfig.json
    
    ## Pourquoi cela est utile ?
    1-/
    2- Afin que les utilisateurs ajoute les produits qu'ils désirent à leurs panier qu'ils retrouveront lors du paiement.
    3- Pour le design
    4- Afin d'avoir des options de couleurs/variants sur la page d'un produit
    5- Afin de tracker les erreurs coté client et utiliser des variables d'environnements
    6- /
    7- Afin que l'utilisateur sélectionne ses préférences facilement
    8- Afin que la résolution des chemin d'import se fasse correctement
    
    ## Comment je l'ai implémenté :
    /
    
    Maintenant, nous avons une page des produits fonctionnelle pour mobile.

---
+++ frontend/src/pages/products.php
@@ -0,0 +1,193 @@
+<?php
+include_once dirname(__FILE__, 2) . '/utils/functions.php';
+
+use function Jegwell\functions\getFileUrl;
+use function Jegwell\functions\createProductsPageModal;
+use Sanity\Client as SanityClient;
+
+$page_css = getFileUrl('../css/products.css', dirname(__FILE__, 2) . '/css/products.css');
+$page_js = getFileUrl('../js/pages/products.js', dirname(__FILE__, 2) . '/js/pages/products.js');
+
+$page_title = 'Jegwell | Créations';
+include '../components/header.php'; // contient le code pour lire les variables d'environnement
+
+$sanity = new SanityClient([
+    'projectId' => $_ENV['SANITY_PROJECT_ID'],
+    'dataset' => 'production',
+    'useCdn' => true,
+    'apiVersion' => $_ENV['SANITY_API_VERSION'],
+]);
+?>
+
+<main>
+    <section class="section">
+        <h1 class="section__h2">NOS CRÉATIONS</h1>
+        <div class="filters">
+            <button class="input-wrapper" id="categoriesButton" data-modal="categoriesModal">
+                <div class="input" data-label="Catégories">
+                    <span>Toutes</span>
+                    <div class="input__caret"></div>
+                </div>
+            </button>
+
+            <button class="input-wrapper" id="sort" data-modal="sortModal">
+                <div class="input" data-label="Trier">
+                    <span>Popularité</span>
+                    <div class="input__caret"></div>
+                </div>
+            </button>
+        </div>
+
+        <div class="products">
+            <?php
+
+            $sortValue = '';
+            if (isset($_GET['trier'])) {
+                switch ($_GET['trier']) {
+                        // case 'nom-a-z':
+                        //     $sortValue = " | order(name)";
+                        //     break;
+                    case 'nom-z-a':
+                        $sortValue = 'name desc';
+                        break;
+                    case 'prix-le-plus-bas':
+                        $sortValue = 'price';
+                        break;
+                    case 'prix-le-plus-haut':
+                        $sortValue = 'price desc';
+                        break;
+
+                    default:
+                        // trier par ordre alphabétique par défaut
+                        $sortValue = 'name';
+                        break;
+                }
+            }
+
+            if (isset($_GET['categories'])) {
+
+                $categoriesFilter = " && count((categories[]->slug.current)[@ in [\"$_GET[categories]\"]]) > 0";
+            }
+
+            $sortFilter = $sortValue == '' ? '' : " | order($sortValue)";
+
+            $query = "
+                    *[_type == 'product' $categoriesFilter] $sortFilter{    
+                        name,
+                        'slug': slug.current,
+                        'image_url': image.asset->url,
+                        price,
+                        options,
+                        _id
+                    }";
+
+
+            if (isset($_GET['categories'])) {
+            }
+
+            $products = $sanity->fetch($query);
+
+            foreach ($products as $product) {
+
+                $options_amount = count($product['options']);
+                $options_html = $options_amount > 0 ? "data-options='$options_amount'" : '';
+                $product_page_url = $_ENV['HOME'] . '/creations/' . $product['slug'];
+                $image_url =  $product['image_url'];
+                $product_name = $product['name'];
+                $price = $product['price'];
+                $product_id = $product['_id'];
+
+                echo "<article class='product'>
+                    <a class='product__image-wrapper' href='$product_page_url' $options_html>
+                        <img class='product__image' src='$image_url' alt='$product_name'>
+                    </a>
+                    <div class='product__information'>
+                        <a class='product__name' href='$product_page_url'>
+                            <span>$product_name</span>
+                        </a>
+                        <span class='product__price'>$price €</span>
+                        <button class='product__call-to-action-wrapper' data-product-id=\"$product_id\">
+                            <span class='product__call-to-action'>Ajouter au panier</span>
+                        </button>
+                    </div>
+                  </article>";
+            }
+            ?>
+
+    </section>
+    <?php
+    // bloc concernant la pagination
+    $total_pages = ceil(count($products) / 10);
+
+    if ($total_pages > 1) {
+
+    ?>
+        <div class="pagination">
+            <?php
+            $previous_page = 1;
+            $next_page = 1;
+            $current_page_index = 1;
+            if (isset($_GET['page'])) {
+                $theoretical_previous_page = $_GET['page'] - 1;
+                $previous_page =  $theoretical_previous_page < 1 ? 1 : $theoretical_previous_page;
+
+                $theoretical_next_page = $_GET['page'] - 1;
+                $next_page =  $theoretical_next_page > $total_pages  ? 1 : $next_previous_page;
+
+                $current_page_index = $_GET['page'];
+            }
+
+
+            $url_parameters = http_build_query(array_merge($_GET, array('page' => 'new_page_to_set')));
+            $current_url = explode("?", $_SERVER['REQUEST_URI'])[0];
+            $previous_page_link = $current_url . '?' . str_replace('new_page_to_set', $previous_page, $url_parameters);
+            $next_page_link = $current_url . '?' . str_replace('new_page_to_set', $next_page, $url_parameters);
+            ?>
+            <a href="<?php echo $previous_page_link ?>" class="pagination__item-wrapper pagination__arrow-wrapper">
+                <div class="pagination__arrow pagination__arrow--left pagination__item"></div>
+            </a>
+
+            <?php
+            for ($page_index = 1; $page_index <= $total_pages; $page_index++) {
+                $page_link = $current_url . '?' . str_replace('new_page_to_set', $page_index, $url_parameters);
+                $additional_class = $current_page_index == $page_index ? 'pagination__item--active' : '';
+                echo "<a href=\"$page_link\" class=\"pagination__item-wrapper\"><span class=\"pagination__item $additional_class\">$page_index</span></a>";
+            }
+            ?>
+
+            <a href="<?php echo $next_page_link ?>" class="pagination__item-wrapper pagination__arrow-wrapper">
+                <div class="pagination__arrow pagination__arrow--right pagination__item"></div>
+            </a>
+        </div>
+
+    <?php
+    }
+    // modals de la page :
+
+    $categories = $sanity->fetch('
+    *[_type == "category"]{    
+        name,
+        "slug": slug.current,
+    }
+');
+
+    $sort_options = array(
+        array('name' => 'Nom A-Z', 'slug' => 'nom-a-z'),
+        array('name' => 'Nom Z-A', 'slug' => 'nom-z-a'),
+        array('name' => 'Prix le plus bas', 'slug' => 'prix-le-plus-bas'),
+        array('name' => 'Prix le plus haut', 'slug' => 'prix-le-plus-haut'),
+
+    );
+
+    // on affiche le html retourné par la fonction. Nous avons maintenant 2 magnifiques modal dans notre DOM.
+    echo createProductsPageModal('categoriesModal', 'Choisissez une catégorie :', $categories, 'categories');
+    echo createProductsPageModal('sortModal', 'Trier par :', $sort_options, 'trier');
+    ?>
+
+
+
+
+</main>
+
+<?php
+include '../components/footer.php';
--- frontend/src/utils/functions.php
+++ frontend/src/utils/functions.php
@@ -36,3 +36,44 @@ function getFileUrl($relativePathToFile, $absolutePath)
     // l'url ne fonctionne que si le nom de domaine est inclu
 
 }
+
+/**
+ * retourne le html de la modal sous forme de string.
+ *
+ * @param string $modalId 
+ * @param string $title 
+ * @param array $items 
+ * 
+ * @return string le html de la modal
+ */
+function createProductsPageModal($modalId, $title, $items, $url_key)
+{
+
+    $svgs_sprite_url = getFileUrl('../assets/svgs-sprite.svg', dirname(__FILE__, 2) . '/assets/svgs-sprite.svg');
+
+    $first_part = "
+    <dialog class=\"modal\" id=\"$modalId\">
+        <button class=\"close-button close-button--modal\">
+        <svg class=\"close-button__cross-icon close-button__cross-icon--modal\">
+            <use href=\"$svgs_sprite_url#cross\" />
+        </svg>
+        </button>
+        <section>
+            <h2 class=\"modal__title\">$title</h2>
+            <ul>
+                ";
+    $second_part = '';
+    foreach ($items as $item) {
+        $url_parameters = http_build_query(array_merge($_GET, array($url_key => $item['slug'])));
+        $current_url = explode("?", $_SERVER['REQUEST_URI'])[0];
+        $second_part = $second_part . "
+            <li class=\"modal__item\"><a class=\"modal__link\" href=\"$current_url?$url_parameters\">$item[name]</a></li>
+            ";
+    }
+    $third_part = '      
+            </ul>
+        </section>
+    </dialog>';
+
+    return $first_part . $second_part . $third_part;
+}
