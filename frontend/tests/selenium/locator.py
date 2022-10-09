be47204754ece31c8785cc1dadde7b7d79bf2a34
Benjamin LESNE <benjamin.lesne@outlook.fr>
2022-10-09 17:29:04 +0200

    feat:JG-52 création tests home page
    ## Quel est la fonctionnalité:
    Des tests end to end pour la page d'accueil
    
    ## Pourquoi cela est utile ?
    Afin de savoir si notre code, fraîchement produit, casse la page d'accueil
    
    ## Comment je l'ai implémenté:
    Avec selenium python et unittest
    
    Maintenant, nous avons des tests end to end qui couvre la page d'accueil.

--- frontend/tests/selenium/locator.py
+++ frontend/tests/selenium/locator.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
-class HomePageLocators(object):
-    MODAL_LINK = (By.CSS_SELECTOR, ".modal__link")
+# class HomePageLocators(object):
+    
+
+class ProductsPageLocators(object):
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".product__call-to-action-wrapper")
+    MODAL_LINK = (By.CSS_SELECTOR, ".modal__link")
 
     
--- frontend/tests/selenium/page.py
+++ frontend/tests/selenium/page.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import ElementNotVisibleException, ElementNotSelectableException
+
 from locator import *
 
 class BasePage(object):
@@ -24,6 +25,106 @@ class BasePage(object):
 
         return string.lower()
 
+class HomePage(BasePage):
+
+    def does_burger_button_open_main_menu(self):
+        burger_button = self.chrome_driver.find_element(By.CSS_SELECTOR, "#burger-button")
+        body = self.chrome_driver.find_element(By.CSS_SELECTOR, "#topOfThePage")
+        main_menu = self.chrome_driver.find_element(By.CSS_SELECTOR, "#main-menu")
+
+        burger_button.click()
+
+        body_inline_style = body.get_attribute("style")
+        is_main_menu_open = main_menu.get_attribute("open")
+
+        if(is_main_menu_open and "overflow: hidden" in body_inline_style):
+            return True
+        else:
+            print('main_menu.get_attribute("open") :')
+            print(main_menu.get_attribute("open"))
+
+            print('"overflow: hidden" in body_inline_style :')
+            print("overflow: hidden" in body_inline_style)
+
+            print("final condition :")
+            print(is_main_menu_open and "overflow: hidden" in body_inline_style)
+
+            return False
+
+    def does_main_menu_display_right_links(self):
+        # a lancer APRES que le main-menu soit ouvert
+        are_results_good = True
+
+        links = self.chrome_driver.find_elements(By.CSS_SELECTOR, ".main-menu__link")
+        base_url = self.chrome_driver.current_url
+        expected_links_href = [
+        base_url + "creations",
+        base_url + "panier",
+        base_url,
+        "http://" + base_url + "#cat%C3%A9gories",
+        ]
+
+        for link in links:
+            href = link.get_attribute("href")
+            if ("http://" + href) not in expected_links_href:
+                are_results_good = False
+                print("href not working (sans http://):")
+                print(href)
+                print("expected_links_href[0]")
+                print(expected_links_href[4])
+                # print( == expected_links_href[4])
+                break
+            
+        return are_results_good
+
+    def does_main_menu_close_properly(self):
+        wait = WebDriverWait(self.chrome_driver, timeout=10, poll_frequency=1, ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+        close_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#main-menu-close-button")))
+
+        # close_button = self.chrome_driver.find_element(By.CSS_SELECTOR, "#main-menu-close-button")
+        main_menu = self.chrome_driver.find_element(By.CSS_SELECTOR, "#main-menu")
+
+        close_button.click()
+
+        is_main_menu_open = main_menu.get_attribute("open")
+
+        if(is_main_menu_open):
+            # debug purposes
+            print("is_main_menu_open :")
+            print(is_main_menu_open)
+
+
+        return is_main_menu_open == None
+        
+    
+
+    def are_categories_links_good(self):
+        # vérifie que les liens vers les catégories sont correctes
+        categoriesLinks = self.chrome_driver.find_elements(By.CSS_SELECTOR, ".categories__link")
+        base_url = self.chrome_driver.current_url
+
+        are_results_good = True
+        expected_links_href = [
+            base_url + "creations?categories=" + "collier",
+            base_url + "creations?categories=" + "bagues",
+            base_url + "creations?categories=" + self.slugify("Boucles d'oreilles"),
+            base_url + "creations?categories=" + "bracelet",
+            base_url + "creations?categories=" + self.slugify("Boite à bijoux"),
+            base_url + "creations?categories=" + "abonnement"]
+
+        for link in categoriesLinks:
+            href = link.get_attribute("href")
+            if ("http://" + href) not in expected_links_href:
+                are_results_good = False
+                print("href not working:")
+                print(href)
+                print("expected_links_href[0]")
+                print(expected_links_href[0])
+                break
+                
+        return are_results_good
+
+
 class ProductsPage(BasePage):
 
     def does_modal_exist(self, buttonId, modalId):
@@ -38,7 +139,7 @@ class ProductsPage(BasePage):
         # vérifie le paramètre de l'url du premier lien de la modal 
         modal = self.chrome_driver.find_element(By.CSS_SELECTOR, f'#{modalId}[open=""]')
 
-        modalLinks = modal.find_elements(*HomePageLocators.MODAL_LINK)
+        modalLinks = modal.find_elements(*ProductsPageLocators.MODAL_LINK)
         results = []
 
         for link in modalLinks:
@@ -69,7 +170,7 @@ class ProductsPage(BasePage):
 
     def does_add_to_basket_create_right_cookie(self):
         # condition initial: aucun cookie productsToBasket présent sur le site
-        add_to_basket_button = self.chrome_driver.find_element(*HomePageLocators.ADD_TO_BASKET_BUTTON)
+        add_to_basket_button = self.chrome_driver.find_element(*ProductsPageLocators.ADD_TO_BASKET_BUTTON)
         
         product_id = add_to_basket_button.get_attribute("data-product-id")
         
@@ -77,6 +178,7 @@ class ProductsPage(BasePage):
         # donc on scroll pour le rendre visible avec JavaScript vu que je n'y arrive pas avec selenium
         self.chrome_driver.execute_script('return arguments[0].scrollIntoView()', add_to_basket_button)
         time.sleep(1) # sans cette ligne, le click se fait trop vite et une erreur est retournée
+        
         add_to_basket_button.click()
         
         basket_cookies = self.chrome_driver.get_cookie("productsToBasket")
@@ -91,7 +193,7 @@ class ProductsPage(BasePage):
             # présent sur le site suite à un premier clique de l'utilisateur
             # sur le bouton ajouter au panier
             # ce code doit être lancé APRES does_add_to_basket_create_right_cookie
-            add_to_basket_button = self.chrome_driver.find_element(*HomePageLocators.ADD_TO_BASKET_BUTTON)
+            add_to_basket_button = self.chrome_driver.find_element(*ProductsPageLocators.ADD_TO_BASKET_BUTTON)
             product_id = add_to_basket_button.get_attribute("data-product-id")
             add_to_basket_button.click()
 
@@ -107,7 +209,7 @@ class ProductsPage(BasePage):
             # présent sur le site suite à un premier clique de l'utilisateur
             # sur le bouton ajouter au panier
             # ce code doit être lancé APRES does_add_to_basket_create_right_cookie
-            add_to_basket_buttons = self.chrome_driver.find_elements(*HomePageLocators.ADD_TO_BASKET_BUTTONs[0])
+            add_to_basket_buttons = self.chrome_driver.find_elements(*ProductsPageLocators.ADD_TO_BASKET_BUTTONs[0])
             if(len(add_to_basket_buttons) > 0):
                 product_id = add_to_basket_buttons[0].get_attribute("data-product-id")
                 index = 1
