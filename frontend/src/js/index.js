846d045a3c7cf0af734071ea6a1dba91c1d1c5ea
BenjaminLesne <73464907+BenjaminLesne@users.noreply.github.com>
2022-10-18 11:24:50 +0200

    feat:JG-42 coder la page panier (#30)
    ## Quel est la fonctionnalité:
    Création de la page panier. Les porduits ajouté y sont affiché, la modification de l'option et de la quantité est possible
    Les tests avec selenium correspondant.
    
    ## Pourquoi cela est utile ?
    /
    
    ## Comment je l'ai implémenté :
    La page est généré a partir des cookies contenant les produits ajoutés. Des attributs html data-product-id/option sont ajouté. Ils servent ensuite lorsqu'on modifie les options/quantité
    
    Maintenant, nous avons une page panier fonctionnelle et testé.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -1,15 +1,4 @@
-import { closeMainMenu, handleAddToBasket } from "./utils/functions.js";
-//import * as dotenv from "../../node_modules/dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// import * as dotenv from "http://localhost:8080/node_modules/dotenv";
-// import * as Sentry from "../../node_modules/@sentry/browser";
-// import { BrowserTracing } from "../../node_modules/@sentry/tracing";
-// dotenv.config();
-// console.log(process.env.SENTRY_JS_DSN);
-// Sentry.init({
-//   dsn: process.env.SENTRY_JS_DSN,
-//   integrations: [new BrowserTracing()],
-//   tracesSampleRate: 1.0,
-// });
+import { closeMainMenu, handleAddToBasket, openQuantityModal, handleQuantity, handleBasketQuantityConfirmation, openOptionsModal, showSelectedOption, handleOptionConfirm } from "./utils/functions.js";
 // ** HEADER **
 const currentRequestUri = location.pathname === "/" ? "/accueil" : location.pathname;
 const linkToActivate = document.querySelector(`[data-request-uri="${currentRequestUri}"]`);
@@ -17,6 +6,13 @@ const burgerButton = document.getElementById("burger-button");
 const mainMenuCloseButton = document.getElementById("main-menu-close-button");
 const mainMenuLinks = document.querySelectorAll(".main-menu__link");
 const addToBasketButtons = document.querySelectorAll(".product__call-to-action-wrapper");
+const modalCloseButtons = document.querySelectorAll(".close-button--modal");
+const quantityButtons = document.querySelectorAll(".setting--quantity");
+const quantitySetterButtons = document.querySelectorAll(".quantity-setter__button");
+const quantityModalConfirmButton = document.querySelector('#quantityModal .main-call-to-action');
+const optionsButtons = document.querySelectorAll(".setting--option");
+const productOptionWrapperAll = document.querySelectorAll(".product-option-wrapper");
+const optionsModalConfirmButtons = document.querySelectorAll(".optionsModal .main-call-to-action");
 // Afin de montrer sur quelle page l'utilisateur est actuellement:
 linkToActivate === null || linkToActivate === void 0 ? void 0 : linkToActivate.classList.add("main-menu__item--active");
 // gère l'apparition/disparition du menu de navigation
@@ -31,4 +27,47 @@ mainMenuLinks === null || mainMenuLinks === void 0 ? void 0 : mainMenuLinks.forE
 });
 addToBasketButtons === null || addToBasketButtons === void 0 ? void 0 : addToBasketButtons.forEach(button => {
     button === null || button === void 0 ? void 0 : button.addEventListener("click", () => handleAddToBasket(button));
+});
+modalCloseButtons === null || modalCloseButtons === void 0 ? void 0 : modalCloseButtons.forEach(button => {
+    button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+        var _a;
+        (_a = button === null || button === void 0 ? void 0 : button.parentElement) === null || _a === void 0 ? void 0 : _a.close();
+    });
+});
+// bouton quantité des produits
+quantityButtons.forEach((button) => {
+    button.addEventListener("click", () => openQuantityModal(button));
+});
+quantitySetterButtons.forEach(button => {
+    var _a;
+    const isAddition = (_a = button.firstElementChild) === null || _a === void 0 ? void 0 : _a.classList.contains("plus");
+    if (isAddition != null) {
+        button.addEventListener("click", () => handleQuantity(isAddition, button));
+    }
+});
+quantityModalConfirmButton === null || quantityModalConfirmButton === void 0 ? void 0 : quantityModalConfirmButton.addEventListener('click', () => handleBasketQuantityConfirmation(quantityModalConfirmButton));
+// boutons option des produits
+optionsButtons.forEach((button) => {
+    button.addEventListener("click", () => openOptionsModal(button));
+});
+productOptionWrapperAll.forEach((button) => {
+    var _a;
+    const productOption = button.getAttribute('data-product-option');
+    const productId = button.getAttribute('data-product-id');
+    const modal = (_a = button.parentElement) === null || _a === void 0 ? void 0 : _a.closest("dialog");
+    if (productOption != null && productId != null && modal != null) {
+        button.addEventListener("click", () => showSelectedOption(productId, productOption, modal));
+    }
+});
+optionsModalConfirmButtons.forEach((button) => {
+    var _a;
+    const modal = (_a = button.parentElement) === null || _a === void 0 ? void 0 : _a.closest('.optionsModal');
+    if (modal != null) {
+        button.addEventListener("click", () => handleOptionConfirm(modal));
+    }
+    else {
+        const message = "modal is undefined or null";
+        console.error(message);
+        throw new Error(message);
+    }
 });
