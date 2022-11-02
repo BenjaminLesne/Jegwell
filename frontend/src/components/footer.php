0db230ccb02c9194315127b0570c2835aab0f996
BenjaminLesne <73464907+BenjaminLesne@users.noreply.github.com>
2022-11-02 10:39:09 +0100

    feat:JG-61 version bureau (#38)
    ## Quel est la fonctionnalité:
    1- Correction bouton backToTop
    2- Section a propos dans le footer commenté
    3- ajout du css pour le bureau avec media query dans le tag link:css
    
    ## Pourquoi cela se produit-il/est utile ?
    1- Aucune certitude, probablement à cause de l'url rewritting avec Apache.
    2- Les liens ne fonctionne pas pour l'instant, autant les retirés de l'interface. Ils fonctionneront lorsque les pages correspondantes seront créées
    3- Afin d'avoir un site responsive
    
    ## Comment je l'ai [corrigé/implémenté] :
    1- J'ai utiliser du javascript dans le href pour scroller au coordonnées 0,0.
    2- /
    3- /
    Maintenant, nous avons un site responsive et un bouton backToTop fonctionnel

--- frontend/src/components/footer.php
+++ frontend/src/components/footer.php
@@ -1,24 +1,25 @@
-<a class="backToTopWrapper clickable" href="#topOfThePage">
+<a class="backToTopWrapper clickable" href="javascript:window.scrollTo(0, 0);">
     <svg class="backToTop">
         <use href="<?php echo $svgs_sprite_url . '#arrow-up' ?>" />
     </svg>
 </a>
 
 <footer class="footer">
-    <h2 class="footer__heading">RÉSEAUX SOCIAUX</h2>
-    <ul class="footer__list">
-        <li class="footer__items"><a class="footer__links" href="<?php echo $instagramUrl ?>">Instagram</a></li>
-        <li class="footer__items"><a class="footer__links" href="<?php echo $tikTokUrl ?>">TikTok</a></li>
-        <li class="footer__items"><a class="footer__links" href="<?php echo $facebookUrl ?>">Facebook</a></li>
-    </ul>
+    <div class="footer__content-wrapper">
+        <h2 class="footer__heading">RÉSEAUX SOCIAUX</h2>
+        <ul class="footer__list">
+            <li class="footer__items"><a class="footer__links" href="<?php echo $instagramUrl ?>">Instagram</a></li>
+            <li class="footer__items"><a class="footer__links" href="<?php echo $tikTokUrl ?>">TikTok</a></li>
+            <li class="footer__items"><a class="footer__links" href="<?php echo $facebookUrl ?>">Facebook</a></li>
+        </ul>
 
-    <h2 class="footer__heading">À PROPOS</h2>
-    <ul class="footer__list">
-        <!-- <li class="footer__items"><a class="footer__links" href="#">CGV</a></li>
-        <li class="footer__items"><a class="footer__links" href="#">Mentions Légales</a></li> -->
-        <li class="footer__items"><a class="footer__links" href="#">Livraison</a></li>
-        <li class="footer__items"><a class="footer__links" href="#">Contact</a></li>
+        <!-- <h2 class="footer__heading">À PROPOS</h2>
+        <ul class="footer__list">
+            <li class="footer__items"><a class="footer__links" href="#">CGV</a></li>
+        <li class="footer__items"><a class="footer__links" href="#">Mentions Légales</a></li>
+            <li class="footer__items"><a class="footer__links" href="#">Livraison</a></li>
+            <li class="footer__items"><a class="footer__links" href="#">Contact</a></li>
 
-    </ul>
-
+        </ul> -->
+    </div>
 </footer>
