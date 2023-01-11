d3a5002d3eb82746c9f4a38ffed38b2a6a66d591
Benjamin LESNE <benjamin.lesne@outlook.fr>
2023-01-11 14:58:21 +0100

    discovering doctrine with mariadb

---
+++ README.md
@@ -0,0 +1,15 @@
+## Installation
+
+- git clone <repository link>
+- yarn install
+- composer install
+
+## How to develop
+
+- yarn server // launch the local server in the public folder
+- yarn watch // launch the webpack server to provide files in the public server on file change
+
+The website should be available in :
+http://localhost:8000/
+
+don't forget to run your local db server
--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,12 @@ class ProductsController extends AbstractController
     #[Route('/creations', name: 'app_products')]
     public function index(): Response
     {
+        $product = new Product();
+        $product->setName('mon premier produit')
+            ->setDescription("ma premier description génial")
+            ->setPrice(3900)
+            ->setSlug('premier-produit');
+
         return $this->render('products/index.html.twig');
     }
 }
