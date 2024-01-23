4ec5beb73252349aee97203fb9e149d2dd3bf208
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-01-23 10:36:05 +0100

    in progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,40 +15,40 @@ import { Footer } from "~/components/Footer/Footer";
 import { useRouter } from "next/router";
 // import { HydrationOverlay } from "@builder.io/react-hydration-overlay";
 
+type Provider = ({ children }: { children: React.ReactNode }) => JSX.Element;
 
-  type Provider = ({ children }: { children: React.ReactNode }) => JSX.Element;
+type ConditionalProviderProps = {
+  condition: boolean;
+  Provider: Provider;
+  children: React.ReactNode;
+};
 
-  type ConditionalProviderProps = {
-    condition: boolean;
-    Provider: Provider;
-    children: React.ReactNode;
-  };
+const ConditionalProvider = ({
+  condition,
+  Provider,
+  children,
+}: ConditionalProviderProps) =>
+  condition ? <Provider>{children}</Provider> : <>{children}</>;
 
-  const ConditionalProvider = ({
-    condition,
-    Provider,
-    children,
-  }: ConditionalProviderProps) =>
-    condition ? <Provider>{children}</Provider> : <>{children}</>;
+const MyApp: AppType = ({ Component, pageProps }) => {
+  const router = useRouter();
+  const isAdmin = router.pathname.includes(BASE_ADMIN_ROUTE);
 
-  const MyApp: AppType = ({ Component, pageProps }) => {
-    const router = useRouter();
-    const isAdmin = router.pathname.includes(BASE_ADMIN_ROUTE);
-
-    return (
-      // <ConditionalProvider
-      //   condition={isDevelopment}
-      //   Provider={HydrationOverlay}
-      // >
-        <Head>
-          <title>{BRAND_NAME}</title>
-        </Head>
-        {isAdmin === false && <Header />}
-        <Component {...pageProps} />
-        {isAdmin === false && <Footer />}
-      // </ConditionalProvider>
-    );
-  };
-
+  return (
+    // <ConditionalProvider
+    //   condition={isDevelopment}
+    //   Provider={HydrationOverlay}
+    // >
+    <>
+      <Head>
+        <title>{BRAND_NAME}</title>
+      </Head>
+      {isAdmin === false && <Header />}
+      <Component {...pageProps} />
+      {isAdmin === false && <Footer />}
+    </>
+    // </ConditionalProvider>
+  );
+};
 
 export default api.withTRPC(MyApp);
